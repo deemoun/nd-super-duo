22e5e2568dbde748d74aa613d83bd4c88267ef6e
deemoun <deemounus@gmail.com>
2016-02-19 20:19:55 -0800

    Fixing crash shareActionProvider crash on rotation

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -30,10 +30,12 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     private final int LOADER_ID = 10;
     private View rootView;
     private String ean;
+    private static String LOG_TAG = "BookDetail";
     private String bookTitle;
     private ShareActionProvider shareActionProvider;
+    private Intent shareIntent;
 
-    public BookDetail(){
+    public BookDetail() {
     }
 
     @Override
@@ -70,7 +72,6 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.book_detail, menu);
-
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
     }
@@ -96,11 +97,16 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+
+        if(shareActionProvider != null) {
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)  + bookTitle);
+            shareActionProvider.setShareIntent(shareIntent);
+        } else {
+            Log.v(LOG_TAG, "Empty shareActionProvider, book name: " + bookTitle);
+        }
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
@@ -109,26 +115,21 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(desc);
 
         String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
-        if(authors != null) {
+        if (authors != null) {
             String[] authorsArr = authors.split(",");
             ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
             ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",", "\n"));
         } else {
-            Log.v("BookDetail", "This book has no author");
+            Log.v(LOG_TAG, "This book has no author");
         }
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
-        if(Patterns.WEB_URL.matcher(imgUrl).matches()){
+        if (Patterns.WEB_URL.matcher(imgUrl).matches()) {
             new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
             rootView.findViewById(R.id.fullBookCover).setVisibility(View.VISIBLE);
         }
 
         String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
         ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
-
-// Back button functionality
-//        if(rootView.findViewById(R.id.right_container)!=null){
-//            rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
-//        }
 
     }
 
@@ -140,7 +141,7 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     @Override
     public void onPause() {
         super.onDestroyView();
-        if(MainActivity.IS_TABLET && rootView.findViewById(R.id.right_container)==null){
+        if (MainActivity.IS_TABLET && rootView.findViewById(R.id.right_container) == null) {
             getActivity().getSupportFragmentManager().popBackStack();
         }
     }
